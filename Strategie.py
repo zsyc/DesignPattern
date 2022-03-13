40839060115fe8cde3c05edb2644b3e7029cd171
zsyc <33007708+zsyc@users.noreply.github.com>
2022-03-13 02:40:49 +0100

    Strategy patter finished ?

--- Strategie.py
+++ Strategie.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractclassmethod
-from re import M
 
 # Interface for fly and quack
 # Fly
@@ -15,6 +14,10 @@ class FlyWithWings(FlyBehavior):
 class FlyNoWay(FlyBehavior):
     def fly(self):
         print("duck cannot fly")        
+
+class FlyRocketPowered(FlyBehavior):
+    def fly(self):
+        print("I'm flying with a rocket!")
 
 # Quack
 class QuackBehavior(ABC):
@@ -38,8 +41,9 @@ class MuteQuack(QuackBehavior):
 class Duck(ABC):
     @abstractclassmethod
     def __init__(self) -> None:
-        self.quackBehavior = QuackBehavior  # 非实例化，而是reference variable, 用于指导子类赋值
+        self.quackBehavior = QuackBehavior  # 非实例化，而是reference variable, 用于指导子类赋值，并且通过装饰器禁止了实体化
         self.flyBehavior = FlyBehavior
+        print("parent")
 
     def swim(self):
         print("all ducks float")
@@ -52,17 +56,32 @@ class Duck(ABC):
 
     def performFly(self):
         self.flyBehavior.fly()
+    
+    def setFlyBehavior(self, flyB):
+        self.flyBehavior = flyB
+        
+    def setQuackBehavior(self, quackB):
+        self.quackBehavior = quackB
 
+print([object in Duck.__bases__,])
+
+# TEST
 # Child class
 class MallardDuck(Duck):
     def __init__(self) -> None:
         self.quackBehavior = Quack()    # 这里有括号，是实例化
         self.flyBehavior = FlyWithWings()
 
-print([object in Duck.__bases__,])
-# test = Duck()
-# test.performQuack()
-mtest = MallardDuck()
-mtest.performQuack()
-mtest.performFly()
+class Modelduck(Duck):
+    def __init__(self):
+        self.flyBehavior = FlyNoWay()
+        self.quackBehavior = Quack()
+    
+    def display(self):
+        print("I'm a model duck")
 
+test = Modelduck()
+test.performQuack()
+test.performFly()
+test.setFlyBehavior(FlyRocketPowered()) # to test the dynamic Setter
+test.performFly()
