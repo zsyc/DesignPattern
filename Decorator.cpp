a160cac9e3e584df135ab259fd66a822bb50b95d
Petra Clemens <33007708+zsyc@users.noreply.github.com>
2023-05-24 02:13:41 +0800

    Decorator with smart pointer

--- Decorator.cpp
+++ Decorator.cpp
@@ -5,9 +5,12 @@
 
 using std::string;
 using std::unique_ptr;
+using std::shared_ptr;
 using std::make_unique;
+using std::make_shared;
 using std::cout;
 using std::endl;
+using std::move;
 
 class Beverage {
 public:
@@ -43,40 +46,40 @@ public:
 
 class Mocha : public CondimentDecorator {
 public:
-    Beverage &beverage;
-    Mocha(Beverage &param_beverage) :
-        beverage(param_beverage) {}
-    string getDescription() const override { return beverage.description + ", Mocha"; }
-    double cost() const override { return 0.2 + beverage.cost(); }
+    unique_ptr<Beverage> sp_beverage;
+    Mocha(unique_ptr<Beverage> param_beverage) :
+        sp_beverage(move(param_beverage)) {}
+    string getDescription() const override { return sp_beverage->getDescription() + ", Mocha"; }
+    double cost() const override { return 0.2 + sp_beverage->cost(); }
 };
 
 class Soy : public CondimentDecorator {
 public:
-    Beverage &beverage;
-    Soy(Beverage &param_beverage) :
-        beverage(param_beverage) {}
-    string getDescription() const override { return beverage.description + ", Soy"; }
-    double cost() const override { return 0.15 + beverage.cost(); }
+    unique_ptr<Beverage> sp_beverage;
+    Soy(unique_ptr<Beverage> param_beverage) :
+        sp_beverage(move(param_beverage)) {}
+    string getDescription() const override { return sp_beverage->getDescription() + ", Soy"; }
+    double cost() const override { return 0.15 + sp_beverage->cost(); }
 };
 
 class Whip : public CondimentDecorator {
 public:
-    Beverage &beverage;
-    Whip(Beverage &param_beverage) :
-        beverage(param_beverage) {}
-    string getDescription() const override { return beverage.description + ", Whip"; }
-    double cost() const override { return 0.10 + beverage.cost(); }
+    unique_ptr<Beverage> sp_beverage;
+    Whip(unique_ptr<Beverage> param_beverage) :
+        sp_beverage(move(param_beverage)) {}
+    string getDescription() const override { return sp_beverage->getDescription() + ", Whip"; }
+    double cost() const override { return 0.10 + sp_beverage->cost(); }
 };
 
 int main() {
-    unique_ptr<Beverage> bev1 = make_unique<Espresso>(); // （）使用无参数constructor
-    cout << bev1->getDescription() << " $" << bev1->cost() << endl;
+    unique_ptr<Beverage> sp_bev1 = make_unique<Espresso>(); // （）使用无参数constructor
+    cout << sp_bev1->getDescription() << " $" << sp_bev1->cost() << endl;
 
-    unique_ptr<Beverage> bev2 = make_unique<HouseBlend>();
-    bev2 = make_unique<Mocha>(*bev2);
-    // bev2 = make_unique<Mocha>(*bev2);
-    // bev2 = make_unique<Whip>(*bev2);
-    cout << bev2->getDescription() << " $" << bev2->cost() << endl;
+    unique_ptr<Beverage> sp_bev2 = make_unique<HouseBlend>();
+    sp_bev2 = make_unique<Mocha>(move(sp_bev2));
+    sp_bev2 = make_unique<Mocha>(move(sp_bev2));
+    sp_bev2 = make_unique<Whip>(move(sp_bev2));
+    cout << sp_bev2->getDescription() << " $" << sp_bev2->cost() << endl;
 
     return 0;
 }
